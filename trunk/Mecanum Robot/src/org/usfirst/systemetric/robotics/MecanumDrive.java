4a9403193c18133cb2924ae67511fb28805c1123
Systemetric <Systemetric@c0861d9e-b0f0-49a4-a42a-b1019a200e9b>
2011-01-24 22:06:57 +0000

    Eric - Not really sure where're I'm going here, but I'll work it out
    git-svn-id: http://systemetric-2011.svn.beanstalkapp.com/source-code@9 c0861d9e-b0f0-49a4-a42a-b1019a200e9b

--- trunk/Mecanum Robot/src/org/usfirst/systemetric/robotics/MecanumDrive.java
+++ trunk/Mecanum Robot/src/org/usfirst/systemetric/robotics/MecanumDrive.java
@@ -5,10 +5,11 @@
 
 package org.usfirst.systemetric.robotics;
 
+import org.usfirst.systemetric.geometry.Matrix;
 import org.usfirst.systemetric.geometry.Vector;
 
 /**
- *
+ * 
  * @author Eric
  */
 public class MecanumDrive implements HolonomicDrive {
@@ -17,40 +18,49 @@ public class MecanumDrive implements HolonomicDrive {
 
 	double speed = 1;
 
-	public MecanumDrive(MecanumWheel[] wheels)
-	{
+	public MecanumDrive(MecanumWheel[] wheels) {
 		this.wheels = wheels;
 		numWheels = wheels.length;
 	}
-	public void setDirection(Vector directionVector)
-	{
-		directionVector = directionVector.unit();
+
+	Vector direction;
+	double heading;
+
+	Vector wheelbaseSize;
+
+	public void setDirection(Vector direction) {
+		this.direction = direction;
+		update();
+	}
+
+	public void setOrientation(float heading) {
+		this.heading = heading;
+		update();
+	}
+
+	private void update() {
+		Vector dir = Matrix.fromRotation(-heading).multiply(direction);
+
 		double[] driveSpeeds = new double[numWheels];
 
-		for(int i = 0; i< numWheels; i++)
-			driveSpeeds[i] = wheels[i].getSpeed(directionVector);
+		for (int i = 0; i < numWheels; i++)
+			driveSpeeds[i] = wheels[i].getSpeed(dir);
 
-		driveSpeeds = fixDriveSpeeds(driveSpeeds);
+		driveSpeeds = normalize(driveSpeeds);
 
-
-		for(int i = 0; i< numWheels; i++)
+		for (int i = 0; i < numWheels; i++)
 			wheels[i].setSpeed(driveSpeeds[i]);
 	}
 
-	private double[] fixDriveSpeeds(double[] speeds)
-	{
+	private double[] normalize(double[] speeds) {
 		double maxSpeed = Double.NEGATIVE_INFINITY;
 
-		for(int i = 0; i< numWheels; i++)
+		for (int i = 0; i < numWheels; i++)
 			maxSpeed = Math.max(Math.abs(speeds[i]), maxSpeed);
 
-		for(int i = 0; i< numWheels; i++)
+		for (int i = 0; i < numWheels; i++)
 			speeds[i] = (speeds[i] / maxSpeed) * speed;
 
 		return speeds;
-	}
-
-	public void setOrientation(float heading)
-	{
 	}
 }
--- trunk/Mecanum Robot/src/org/usfirst/systemetric/robotics/OrthogonalMecanumDrive.java
+++ trunk/Mecanum Robot/src/org/usfirst/systemetric/robotics/OrthogonalMecanumDrive.java
@@ -1,5 +1,6 @@
 package org.usfirst.systemetric.robotics;
 
+import org.usfirst.systemetric.geometry.Matrix;
 import org.usfirst.systemetric.geometry.Vector;
 
 import edu.wpi.first.wpilibj.SpeedController;
@@ -9,29 +10,29 @@ public class OrthogonalMecanumDrive implements HolonomicDrive {
 	SpeedController frontLeft;
 	SpeedController backLeft;
 	SpeedController backRight;
-	
+
 	Vector direction;
 	double heading;
-	
-	Vector wheelbaseSize;
-	
-	
-	
-	public void setDirection(Vector d) {
-		byte syncGroup = 0x02;
-		
-		frontRight.set(d.y - d.x, syncGroup);
-		frontRight.set(d.y - d.x, syncGroup);
 
+	Vector wheelbaseSize;
+
+	public void setDirection(Vector direction) {
+		this.direction = direction;
+		update();
 	}
 
 	public void setOrientation(float heading) {
-		// TODO Auto-generated method stub
-
+		this.heading = heading;
+		update();
 	}
-	
+
 	private void update() {
-		
+		Vector dir = Matrix.fromRotation(heading).multiply(direction);
+
+		byte syncGroup = 0x02;
+
+		frontRight.set(dir.y - dir.x, syncGroup);
+
 	}
 
 }
