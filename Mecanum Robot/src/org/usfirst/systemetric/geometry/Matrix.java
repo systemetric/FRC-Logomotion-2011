a16109f5e119e46cc7a44400b077a415fd53ed97
Systemetric <Systemetric@c0861d9e-b0f0-49a4-a42a-b1019a200e9b>
2011-01-24 20:41:16 +0000

    Added javadoc
    git-svn-id: http://systemetric-2011.svn.beanstalkapp.com/source-code@6 c0861d9e-b0f0-49a4-a42a-b1019a200e9b

--- Mecanum Robot/src/org/usfirst/systemetric/geometry/Matrix.java
+++ Mecanum Robot/src/org/usfirst/systemetric/geometry/Matrix.java
@@ -1,81 +1,135 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.usfirst.systemetric.geometry;
 
 /**
- *
- * @author Robotics
+ * 
+ * @author Eric
+ * 
+ *         Represents a 2 by 2 Matrix for use in geometric transformations
  */
-public class Matrix
-{
+public class Matrix {
+	/**
+	 * The identity matrix
+	 * 
+	 * <pre>
+	 * [ 1 0 ]
+	 * [ 0 1 ]
+	 * </pre>
+	 */
+	public static Matrix IDENTITY = new Matrix(1, 0, 0, 1);
+	/**
+	 * 90 Degree counterclockwise rotation
+	 */
+	public static Matrix ROTATE90 = new Matrix(0, 1, -1, 0);
+	/**
+	 * 180 Degree counterclockwise rotation
+	 */
+	public static Matrix ROTATE180 = new Matrix(-1, 0, 0, -1);
+	/**
+	 * 270 Degree counterclockwise rotation
+	 */
+	public static Matrix ROTATE270 = new Matrix(0, -1, 1, 0);
+
+	/**
+	 * Creates a transformation matrix describing a rotation of the given angle
+	 * @param angle angle in Radians
+	 */
+	public static Matrix fromRotation(double angle) {
+		double sinA = Math.sin(angle);
+		double cosA = Math.cos(angle);
+		return new Matrix(cosA, sinA, -sinA, cosA);
+	}
 
 	public final double a, b, c, d;
-	/*
-	 *  [ a  b ]
-	 *  [ c  d ]
-	 */
 
-	public Matrix(double a, double b, double c, double d)
-	{
+	/**
+	 * Construct a square 2x2 Matrix of the form
+	 * 
+	 * <pre>
+	 * [ a b ]
+	 * [ c d ]
+	 * </pre>
+	 */
+	public Matrix(double a, double b, double c, double d) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 		this.d = d;
 	}
 
-	public Matrix(Vector xAxis, Vector yAxis)
-	{
+	/**
+	 * Construct a square 2x2 Matrix representing the transformation onto a new
+	 * pair of coordinate axes
+	 */
+	public Matrix(Vector xAxis, Vector yAxis) {
 		this(xAxis.x, yAxis.x, xAxis.y, yAxis.y);
 	}
 
-	public Matrix multiply(double k)
-	{
-		return new Matrix(a*k, b*k, c*k, d*k);
+	/**
+	 * Multiply each cell of the Matrix by a constant
+	 * 
+	 * @param k
+	 *            constant to multiply by
+	 */
+	public Matrix multiply(double k) {
+		return new Matrix(a * k, b * k, c * k, d * k);
 	}
 
-	public Vector multiply(Vector v)
-	{
+	/**
+	 * Divide each cell of the Matrix by a constant
+	 * 
+	 * @param k
+	 *            constant to divide by
+	 */
+	public Matrix divide(double k) {
+		return new Matrix(a / k, b / k, c / k, d / k);
+	}
+
+	/**
+	 * Transforms the vector using the Matrix
+	 * 
+	 * @param v
+	 *            vector to transform
+	 */
+	public Vector multiply(Vector v) {
 		return new Vector(a * v.x + b * v.y, c * v.x + d * v.y);
 	}
 
-	public Matrix multiply(Matrix that)
-	{
-		return new Matrix(a * that.a + b * that.c, a * that.b + b * that.d,
-						  c * that.a + d * that.c, c * that.b + d * that.d);
+	/**
+	 * Creates a compound transformation by combining matrices
+	 * 
+	 * @param that
+	 *            matrix to multiply by
+	 */
+	public Matrix multiply(Matrix that) {
+		return new Matrix(a * that.a + b * that.c, a * that.b + b * that.d, c
+				* that.a + d * that.c, c * that.b + d * that.d);
 	}
 
-	public double determinant()
-	{
-		return a*d - b*c;
+	/**
+	 * The determinant of the matrix, <code>ad - bc</code>
+	 */
+	public double determinant() {
+		return a * d - b * c;
 	}
 
-	public Matrix inverse()
-	{
-		return new Matrix(d, -c, -b, a).multiply(1/determinant());
+	/**
+	 * The inverse of the matrix, representing the reverse transformation
+	 */
+	public Matrix inverse() {
+		return new Matrix(d, -c, -b, a).divide(determinant());
 	}
 
-	public String toString()
-	{
-		return "matrix:\n" + a + " " + b + "\n" + c + " " + d;
+	public String toString() {
+		return "Matrix: [" + a + "," + b + "][" + c + "," + d + "]]";
 	}
 
-	public boolean equals(Object obj)
-	{
-		if(obj == null || !(obj instanceof Matrix))
-		{
+	public boolean equals(Object obj) {
+		if (obj == null || !(obj instanceof Matrix)) {
 			return false;
 		}
 
 		Matrix that = (Matrix) obj;
 		return a == that.a && b == that.b && c == that.c && d == that.d;
 
-	}
-
-	public static Matrix fromRotation(double angle)
-	{
-		return new Matrix(Math.cos(angle), Math.sin(angle),
-						  -Math.sin(angle), Math.cos(angle));
 	}
 }
