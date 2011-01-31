a1c7bc265e6fea4527a3925dcbb6eaffe577fd9b
Systemetric <Systemetric@c0861d9e-b0f0-49a4-a42a-b1019a200e9b>
2011-01-31 17:32:20 +0000

    BLINKY LEDS!!!1!
    git-svn-id: http://systemetric-2011.svn.beanstalkapp.com/source-code@21 c0861d9e-b0f0-49a4-a42a-b1019a200e9b

--- trunk/Mecanum Robot/src/org/usfirst/systemetric/RobotTest.java
+++ trunk/Mecanum Robot/src/org/usfirst/systemetric/RobotTest.java
@@ -5,30 +5,66 @@ import org.usfirst.systemetric.robotics.Arm;
 import org.usfirst.systemetric.robotics.Grabber;
 import org.usfirst.systemetric.robotics.navigation.MecanumDrive;
 
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.CANJaguar.ControlMode;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStationEnhancedIO;
+import edu.wpi.first.wpilibj.DriverStationEnhancedIO.EnhancedIOException;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SmartDashboard;
 import edu.wpi.first.wpilibj.SmartDashboardPacketFactory;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
+
 
 public class RobotTest extends IterativeRobot {
 	GenericHID driveJoystick;
 	GenericHID armJoystick;
+	DriverStationEnhancedIO io;
 
 	Arm arm;
-	//MecanumDrive mecanumDrive;
 	
-	Controllable grabberController = new GrabberController(new Grabber(1, 2));
-	//Controllable driveController = new DriveController(mecanumDrive);
+	CANJaguar jag;
+	// MecanumDrive mecanumDrive;
 
+	Controllable grabberController = new GrabberController(new Grabber(1, 2));
+
+	// Controllable driveController = new DriveController(mecanumDrive);
 
 	public void teleopInit() {
-		//driveJoystick = new Joystick(1);
+		// driveJoystick = new Joystick(1);
 		armJoystick = new Joystick(1);
+		io = DriverStation.getInstance().getEnhancedIO();
+		try {
+			jag = new CANJaguar(2, ControlMode.kPercentVbus);
+		} catch (CANTimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
+
+	boolean ledState = false;
 
 	public void teleopPeriodic() {
 		grabberController.controlWith(armJoystick);
-		//driveController.controlWith(driveJoystick);
+		// driveController.controlWith(driveJoystick);
+		try {
+			if (io.getDigital(1)) {
+				ledState = !ledState;
+				io.setLED(1, ledState);
+				jag.setX(1);
+			}
+			else 
+				jag.setX(-0.5);
+				
+		} catch (EnhancedIOException e) {
+			e.printStackTrace();
+		} catch (CANTimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	};
 }
+
