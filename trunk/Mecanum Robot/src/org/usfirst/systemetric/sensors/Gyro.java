b35cb589c37d917ba9a8b58549298f91581fbfdc
Systemetric <Systemetric@c0861d9e-b0f0-49a4-a42a-b1019a200e9b>
2011-02-03 17:08:54 +0000

    New sensors to fix incorrect implementations.
    git-svn-id: http://systemetric-2011.svn.beanstalkapp.com/source-code@23 c0861d9e-b0f0-49a4-a42a-b1019a200e9b

---
+++ trunk/Mecanum Robot/src/org/usfirst/systemetric/sensors/Gyro.java
@@ -0,0 +1,20 @@
+package org.usfirst.systemetric.sensors;
+
+import org.usfirst.systemetric.utils.AngleFinder;
+import edu.wpi.first.wpilibj.AnalogChannel;
+
+public class Gyro extends edu.wpi.first.wpilibj.Gyro implements AngleFinder {
+
+	public Gyro(AnalogChannel channel) {
+		super(channel);
+	}
+
+	public Gyro(int slot, int channel) {
+		super(slot, channel);
+	}
+
+	public Gyro(int channel) {
+		super(channel);
+	}
+
+}
