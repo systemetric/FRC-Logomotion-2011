b7f48ab0274a3d06f8d874f2c76dc5e2c5bbf1f0
Systemetric <Systemetric@c0861d9e-b0f0-49a4-a42a-b1019a200e9b>
2011-02-03 17:09:18 +0000

    Useful classes
    git-svn-id: http://systemetric-2011.svn.beanstalkapp.com/source-code@24 c0861d9e-b0f0-49a4-a42a-b1019a200e9b

---
+++ trunk/Mecanum Robot/src/org/usfirst/systemetric/utils/AccelerometerAccumulator.java
@@ -0,0 +1,35 @@
+package org.usfirst.systemetric.utils;
+
+import org.usfirst.systemetric.geometry.Vector;
+import org.usfirst.systemetric.sensors.ADXL345_I2C;
+
+public class AccelerometerAccumulator {
+
+	ADXL345_I2C accelerometer;
+	long lastTime;
+	Vector velocity;
+
+	public AccelerometerAccumulator(ADXL345_I2C acc) {
+		super();
+		this.accelerometer = acc;
+		reset();
+	}
+
+	public void update() {
+		long currentTime = System.currentTimeMillis();
+
+		long dt = currentTime - lastTime;
+		lastTime = currentTime;
+		velocity = velocity.add(accelerometer.getHorizontalAcceleration().scale(
+				dt / 1000.0));
+	}
+
+	public Vector getVelocity() {
+		return velocity;
+	}
+
+	public void reset() {
+		velocity = new Vector(0, 0);
+		lastTime = System.currentTimeMillis();
+	}
+}
---
+++ trunk/Mecanum Robot/src/org/usfirst/systemetric/utils/AngleFinder.java
@@ -0,0 +1,6 @@
+package org.usfirst.systemetric.utils;
+
+public interface AngleFinder {
+	/** return the angle, in degrees. Use Double.NaN as an exceptional value */
+	public double getAngle();
+}
