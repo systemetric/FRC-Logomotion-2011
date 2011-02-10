01ebf716d650c2498096d0f9803767c2446137d8
Systemetric <Systemetric@c0861d9e-b0f0-49a4-a42a-b1019a200e9b>
2011-02-10 16:19:25 +0000

    Simpler constructor
    git-svn-id: http://systemetric-2011.svn.beanstalkapp.com/source-code@48 c0861d9e-b0f0-49a4-a42a-b1019a200e9b

--- trunk/Mecanum Robot/src/org/usfirst/systemetric/util/OrthogonalMecanumDriveFactory.java
+++ trunk/Mecanum Robot/src/org/usfirst/systemetric/util/OrthogonalMecanumDriveFactory.java
@@ -6,6 +6,9 @@ import org.usfirst.systemetric.robotics.navigation.MecanumDrive;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
 
 public class OrthogonalMecanumDriveFactory {
+	
+	static final double SQRT_0_5 = Math.sqrt(0.5);
+	
 	public static MecanumDrive createMecanumDrive(Vector size,
 	    double wheelRadius, double gearRatio) throws CANTimeoutException {
 
@@ -14,7 +17,27 @@ public class OrthogonalMecanumDriveFactory {
 		Vector leftDriveVector = new Vector(0, -distancePerRotation);
 		Vector offset = size.divideBy(2);
 
-		final double SQRT_0_5 = Math.sqrt(0.5);
+
+		return new MecanumDrive(new MecanumDrive.Wheel[] {
+		        new MecanumDrive.Wheel(new Vector(offset.x, offset.y),
+		            leftDriveVector, new Vector(SQRT_0_5, SQRT_0_5),
+		            JaguarFactory.createSpeedController(2)),
+		        new MecanumDrive.Wheel(new Vector(offset.x, -offset.y),
+		            leftDriveVector, new Vector(SQRT_0_5, -SQRT_0_5),
+		            JaguarFactory.createSpeedController(3)),
+		        new MecanumDrive.Wheel(new Vector(-offset.x, -offset.y),
+		            rightDriveVector, new Vector(-SQRT_0_5, -SQRT_0_5),
+		            JaguarFactory.createSpeedController(4)),
+		        new MecanumDrive.Wheel(new Vector(-offset.x, offset.y),
+		            rightDriveVector, new Vector(-SQRT_0_5, SQRT_0_5),
+		            JaguarFactory.createSpeedController(5))});
+	}
+	
+	public static MecanumDrive createMecanumDrive(Vector size) throws CANTimeoutException {
+
+		Vector rightDriveVector = new Vector(0, 1);
+		Vector leftDriveVector = new Vector(0, -1);
+		Vector offset = size.divideBy(2);
 
 		return new MecanumDrive(new MecanumDrive.Wheel[] {
 		        new MecanumDrive.Wheel(new Vector(offset.x, offset.y),
